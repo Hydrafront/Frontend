830aa69807d89289d7f704cbdfba88adac03d78d
Lightcomet53 <lightcomet777@gmail.com>
2025-03-19 17:52:38 +0200

    update: socket config

--- src/socket/token.ts
+++ src/socket/token.ts
@@ -1,8 +1,19 @@
 import { TokenType, TransactionType } from "@/interfaces/types";
 import { io } from "socket.io-client";
 
-const socket = io(`${import.meta.env.VITE_SERVER_URL}`);
-
+const socket = io(`${import.meta.env.VITE_SERVER_URL}`, {
+  transports: ['websocket'],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  path: '/socket.io/',
+  autoConnect: true,
+  forceNew: true,
+  withCredentials: true,
+  extraHeaders: {
+    "Access-Control-Allow-Origin": "*"
+  }
+});
 
 export default socket;
 
@@ -10,8 +21,16 @@ socket.on("connect", () => {
   console.log("connected to socket");
 });
 
-socket.on("disconnect", () => {
-  console.log("disconnected from socket");
+socket.on("connect_error", (error) => {
+  console.error("Socket connection error:", error);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("disconnected from socket:", reason);
+});
+
+socket.on("error", (error) => {
+  console.error("Socket error:", error);
 });
 
 export const createTokenEmit = (token: TokenType) => {
